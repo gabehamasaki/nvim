f0a3b4326311029629520417303cdfa19d53981a
Gabriel Hamasaki <gabriel.hamasaki@exata.it>
2024-08-08 12:10:37 -0300

    feat: 🎸 configure copilot

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -267,7 +267,22 @@ local default_plugins = {
   cmd = "Copilot",
       event = "InsertEnter",
   config = function()
-    require("copilot").setup({})
+    require("copilot").setup({
+                  suggestion = {
+    enabled = true,
+    auto_trigger = false,
+    hide_during_completion = true,
+    debounce = 75,
+    keymap = {
+      accept = "<C-CR>",
+      accept_word = false,
+      accept_line = false,
+      next = "<M-]>",
+      prev = "<M-[>",
+      dismiss = "<C-]>",
+    },
+  },
+            })
   end,
 },
 }
